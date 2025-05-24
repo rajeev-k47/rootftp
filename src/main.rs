f20a43de410eebb91b8ff9f12a6eb1d0fd1e3cad
rajeev-k47 <rajeevkumawat2007@outlook.com>
2025-05-25 00:19:56 +0530

    <Imp:Implements auth from Authenticator method>?

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,23 @@ use std::sync::Arc;
 use unftp_auth_jsonfile::JsonFileAuthenticator;
 use unftp_sbe_fs::ServerExt;
 use std::path::PathBuf;
+mod auth;
+use auth::SimpleAuthenticator;
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     //pretty_env_logger::init();
 
  let authenticator = JsonFileAuthenticator::from_file(String::from(std::env::current_dir().unwrap().join("credentials.json").to_str().unwrap()))?;
-
+let path = PathBuf::from(String::from(std::env::current_dir().unwrap().join("credentials.json").to_str().unwrap()));
+    let auth = SimpleAuthenticator::new(path);
+//for user in auth.list_users() {
+//        println!("User: {:?}", user);
+ //   }
+ //   let auth
  let ftp_home = PathBuf::from("/home/rajeev/Documents/");
     let addr = "192.168.221.160:2121";
     let server = libunftp::Server::with_fs(ftp_home)
-        .authenticator(Arc::new(authenticator))
+        .authenticator(Arc::new(auth))
         .build()
         .unwrap();
 
