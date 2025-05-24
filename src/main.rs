76f30ffc287ff66df452561c1605664d22c3affa
rajeev-k47 <rajeevkumawat2007@outlook.com>
2025-05-25 04:08:24 +0530

    <Refac:*/*>?

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 use std::sync::Arc;
-use unftp_auth_jsonfile::JsonFileAuthenticator;
 use unftp_sbe_fs::ServerExt;
 use std::path::PathBuf;
 mod auth;
-use auth::SimpleAuthenticator;
+mod global_consts;
+use global_consts::SimpleAuthenticator;
 use local_ip_address::local_ip;
-
+mod init;
+use init::init;
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    
+    init()?;
     let ip = local_ip().unwrap();
     //with direct json auth
     //let authenticator = JsonFileAuthenticator::from_file(String::from(std::env::current_dir().unwrap().join("credentials.json").to_str().unwrap()))?;
@@ -17,7 +18,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let auth = SimpleAuthenticator::new(path);
 
 
-    let ftp_home = PathBuf::from("/home/rajeev/Documents/");
+    let ftp_home = PathBuf::from("/home/rajeev/ftpd");
 
         let addr = format!("{}:{}",ip,2121);
         let server = libunftp::Server::with_fs(ftp_home)
