1c7b9fa4961caeff7a7215929a07c02cc0d44c7f
rajeev-k47 <rajeevkumawat2007@outlook.com>
2025-05-25 01:48:04 +0530

    <Imp:Adds dynamic addr invo. & custom authenticator>?

--- src/main.rs
+++ src/main.rs
@@ -4,25 +4,28 @@ use unftp_sbe_fs::ServerExt;
 use std::path::PathBuf;
 mod auth;
 use auth::SimpleAuthenticator;
+use local_ip_address::local_ip;
+
+
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    //pretty_env_logger::init();
-
- let authenticator = JsonFileAuthenticator::from_file(String::from(std::env::current_dir().unwrap().join("credentials.json").to_str().unwrap()))?;
-let path = PathBuf::from(String::from(std::env::current_dir().unwrap().join("credentials.json").to_str().unwrap()));
+    
+    let ip = local_ip().unwrap();
+    //with direct json auth
+    //let authenticator = JsonFileAuthenticator::from_file(String::from(std::env::current_dir().unwrap().join("credentials.json").to_str().unwrap()))?;
+    let path = PathBuf::from(String::from(std::env::current_dir().unwrap().join("credentials.json").to_str().unwrap()));
     let auth = SimpleAuthenticator::new(path);
-//for user in auth.list_users() {
-//        println!("User: {:?}", user);
- //   }
- //   let auth
- let ftp_home = PathBuf::from("/home/rajeev/Documents/");
-    let addr = "192.168.221.160:2121";
-    let server = libunftp::Server::with_fs(ftp_home)
-        .authenticator(Arc::new(auth))
-        .build()
-        .unwrap();
 
-    server.listen(addr).await?;
 
-    Ok(())
+    let ftp_home = PathBuf::from("/home/rajeev/Documents/");
+
+        let addr = format!("{}:{}",ip,2121);
+        let server = libunftp::Server::with_fs(ftp_home)
+            .authenticator(Arc::new(auth))
+            .build()
+            .unwrap();
+
+        server.listen(addr).await?;
+
+        Ok(())
 }
