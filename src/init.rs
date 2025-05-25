a5eaa8a91020670a97e9dc027b716bc797b4e226
rajeev-k47 <rajeevkumawat2007@outlook.com>
2025-05-25 07:49:32 +0530

    <New:InitBlock for shared dir>?

--- src/init.rs
+++ src/init.rs
@@ -1,9 +1,17 @@
 use std::fs;
 use std::io;
-
+use crate::config::Config;
 
 pub fn init() -> io::Result<()> {
-    fs::create_dir_all("/home/rajeev/ftpd/shared")?;
+    
+    let config = Config::load();
+    let shared_dir = config.root_dir.clone()
+    .join("ftpd")
+    .join("shared");
+
+    //print!("223423424 :{}", shared_dir.display());
+    fs::create_dir_all(&shared_dir)?;
+
     Ok(())
 }
 
