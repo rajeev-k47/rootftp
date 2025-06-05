cfd41119d6fa0ec796144619c4c778174d549f9d
rajeev-k47 <rajeevkumawat2007@outlook.com>
2025-06-06 03:40:30 +0530

    <Refac:*/*>?

--- src/init.rs
+++ src/init.rs
@@ -1,17 +1,19 @@
+use crate::config::Config;
 use std::fs;
 use std::io;
-use crate::config::Config;
 
 pub fn init() -> io::Result<()> {
-    
     let config = Config::load();
-    let shared_dir = config.root_dir.clone()
-    .join("ftpd")
-    .join("shared");
+    let root_dir = config.root_dir.clone();
+    let ftpd = root_dir.join("ftpd");
+    let credentials = root_dir.join("credentials.json");
+    fs::create_dir_all(&root_dir)?;
+    fs::create_dir_all(&ftpd)?;
+    fs::File::create(&credentials)?;
+    //let shared_dir = config.root_dir.clone().join("ftpd").join("shared");
 
     //print!("223423424 :{}", shared_dir.display());
-    fs::create_dir_all(&shared_dir)?;
+    //fs::create_dir_all(&shared_dir)?;
 
     Ok(())
 }
-
